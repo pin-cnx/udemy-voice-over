e4d8156dd061e9891e7cdc025f7c5983b647c790
Pitipong Guntawong <pppstudio.gm@gmail.com>
2020-09-18 01:18:28 +0700

    tweak tts domain

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,3 +1,5 @@
+let ENGINE = 'wavenet' // wavenet,tts
+
 class Queue {
   constructor () {
     this.data = []
@@ -38,96 +40,147 @@ function fetchVoice () {
       skip--
     } else {
       while (text[text.length - 1] !== '.' && text[text.length - 1] !== '?' && text[text.length - 1] !== ',') {
+        if (a.parent().next().length === 0) break
         a = a.parent().next().children()
         text = text + ' ' + a.text()
         skip++
         if (skip > 10) return
       }
 
-      // speechSynthesis text to speech
-      // --------------------------------
-      queue.enqueue({text: text})
-
-      // Google text to speech API
-      // --------------------------------
-      // if (cache_text === text) {
-      //   console.log('queue cache', text)
-      //   cache_data.text = text
-      //   queue.enqueue(cache_data)
-      //   //var snd = new Audio('data:audio/x-wav;base64, ' + cache_data.audioContent)
-      //   //snd.play()
-      // } else {
-      //   jQuery('[data-purpose=\'transcript-cue-active\']').css('color', 'red')
-      //   jQuery.post('http://localhost:8000/api/translate', {text: text}, function (data, status) {
-      //     if (enable_play) {
-      //       data.text = text
-      //       queue.enqueue(data)
-      //       console.log('queue fetch', text)
-      //       //var snd = new Audio('data:audio/x-wav;base64, ' + data.audioContent)
-      //       //snd.play()
-      //     }
-      //   }, 'json')
-      // }
-      // --------------------------------
+      if (ENGINE === 'wavenet') {
+        // Google text to speech API
+        // --------------------------------
+        fetchNextVoice(a).then(function () {})
+        if (cache_text === text) {
+          console.log('QUEUE[CACHE]', text)
+          if (!cache_data.hasOwnProperty('engine')) {
+            cache_data.engine = 'wavenet'
+          }
+          if (!cache_data.hasOwnProperty('text')) {
+            cache_data.text = text
+          }
+          queue.enqueue(cache_data)
+          //var snd = new Audio('data:audio/x-wav;base64, ' + cache_data.audioContent)
+          //snd.play()
+        } else {
+          jQuery('[data-purpose=\'transcript-cue-active\']').css('color', 'red')
+          callT2S(text, function (data, status) {
+            if (enable_play) {
+              if (!data.hasOwnProperty('text')) {
+                data.text = text
+              }
+              if (!data.hasOwnProperty('engine')) {
+                data.engine = 'wavenet'
+              }
+              queue.enqueue(data)
+              console.log('QUEUE[FETCH]', text)
+              //var snd = new Audio('data:audio/x-wav;base64, ' + data.audioContent)
+              //snd.play()
+            }
+          }, (error) => {queue.enqueue({text: text, engine: 'tts'})})
+        }
+        // --------------------------------
+      } else {
+        // speechSynthesis text to speech
+        // --------------------------------
+        queue.enqueue({text: text})
+      }
     }
   }
 
   setTimeout('fetchVoice()', 50)
 }
 
+let overlapCall = {}
+
+function callT2S (text, callback, errorCallback = null) {
+  if (overlapCall.hasOwnProperty(text)) {
+    console.log('OVERLAP CALL')
+    overlapCall[text].push(callback)
+  } else {
+    overlapCall[text] = [callback]
+    jQuery.ajax({
+      'type': 'POST',
+      'url': 'https://tts.chiangmaioutsource.com/api/translate',
+      'contentType': 'application/json',
+      'data': JSON.stringify({input: {text: text}}),
+      'dataType': 'json',
+      'success': (data, status) => {
+        for (let i in overlapCall[text]) {
+          overlapCall[text][i](data, status)
+        }
+        delete overlapCall[text]
+      },
+      'error': function (error) {
+        console.error(error)
+        // ENGINE = 'tts'
+        delete overlapCall[text]
+        if (errorCallback) {errorCallback(error)}
+      }
+    })
+  }
+}
+
 // Google text to speech API
 // --------------------------------
-// if (cache_text === text) {
-// async function fetchNextVoice (a) {
-//   a = a.parent().next().children()
-//   let text = a.text()
-//   while (text[text.length - 1] !== '.' && text[text.length - 1] !== '?' && text[text.length - 1] !== ',') {
-//     a = a.parent().next().children()
-//     text = text + ' ' + a.text()
-//   }
-//
-//   jQuery.post('http://localhost:8000/api/translate', {text: text}, function (data, status) {
-//     console.log('prepared', text)
-//     cache_text = text
-//     cache_data = data
-//   }, 'json')
-// }
-// --------------------------------
+async function fetchNextVoice (a) {
+  a = a.parent().next().children()
+  let text = a.text()
+  while (text[text.length - 1] !== '.' && text[text.length - 1] !== '?' && text[text.length - 1] !== ',') {
+    if (a.parent().next().length === 0) break
+    a = a.parent().next().children()
+    text = text + ' ' + a.text()
+  }
+  callT2S(text, function (data, status) {
+    console.log('PRELOAD', text)
+    cache_text = text
+    cache_data = data
+  })
+}
 
+// --------------------------------
 
 function autoQueuePlay () {
   if (queue.isEmpty()) {
     setTimeout('autoQueuePlay()', 10)
   } else {
     let data = queue.dequeue()
-    console.log('play queue', data.text)
 
-    // Google text to speech API
-    // --------------------------------
-    // var snd = new Audio('data:audio/x-wav;base64, ' + data.audioContent)
-    // snd.onended = function () {
-    //   autoQueuePlay()
-    // }
-    // snd.play()
+    let engine = data.engine ? data.engine : ENGINE
+    let lang = data.lang ? data.lang : 'en-US'
 
-    // speechSynthesis text to speech
-    // --------------------------------
-    var voices = window.speechSynthesis.getVoices()
-    var msg = new SpeechSynthesisUtterance()
-    msg.voice = voices[0] // Note: some voices don't support altering params
-    msg.voiceURI = 'native'
-    msg.volume = 1 // 0 to 1
-    msg.rate = 1 // 0.1 to 10
-    msg.pitch = 0 //0 to 2
-    msg.text = data.text
-    msg.lang = 'en-US'
+    console.log('PLAY[QUEUE]', [engine, lang, data.text])
 
-    msg.onend = function (e) {
-      autoQueuePlay()
+    if (engine === 'wavenet') {
+      // Google text to speech API
+      // --------------------------------
+      var snd = new Audio('data:audio/x-wav;base64, ' + data.audioContent)
+      const nextPlay = function () {
+        autoQueuePlay()
+      }
+      snd.onended = nextPlay
+      snd.onerror = nextPlay
+      snd.play()
+    } else {
+      // speechSynthesis text to speech
+      // --------------------------------
+      // var voices = window.speechSynthesis.getVoices()
+      var msg = new SpeechSynthesisUtterance()
+      // msg.voice = voices[0] // Note: some voices don't support altering params
+      msg.voiceURI = 'native'
+      msg.volume = 1 // 0 to 1
+      msg.rate = 1 // 0.1 to 10
+      msg.pitch = 0 //0 to 2
+      msg.text = data.text
+      msg.lang = lang
+
+      msg.onend = function (e) {
+        autoQueuePlay()
+      }
+      speechSynthesis.speak(msg)
+      // --------------------------------
     }
-    // --------------------------------
 
-    speechSynthesis.speak(msg)
   }
 
 }
